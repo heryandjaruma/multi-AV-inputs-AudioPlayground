81573a141eb0348b6a9047514711c4daae77ca73
RyanDJ <heryandjaruma@gmail.com>
2026-09-10 15:17:41 +0800

    Initial Commit

---
+++ AudioPlayground/AudioPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  AudioPlaygroundApp.swift
+//  AudioPlayground
+//
+//  Created by Heryan Djaruma on 10/09/26.
+//
+
+import SwiftUI
+
+@main
+struct AudioPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AudioPlayground/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AudioPlayground
+//
+//  Created by Heryan Djaruma on 10/09/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
